da7a90fe0c54a5252fc9aacc2f6e594725e863ee
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-02-06 18:48:11 +0800

    added HasWebsiteable to HasProfile Trait

--- src/Traits/HasProfile.php
+++ src/Traits/HasProfile.php
@@ -5,11 +5,12 @@ namespace CleaniqueCoders\Profile\Traits;
 use CleaniqueCoders\Profile\Traits\Morphs\Addressable;
 use CleaniqueCoders\Profile\Traits\Morphs\Emailable;
 use CleaniqueCoders\Profile\Traits\Morphs\Phoneable;
+use CleaniqueCoders\Profile\Traits\Morphs\Websiteable;
 
 /**
  * HasProfile Trait
  */
 trait HasProfile
 {
-    use Addressable, Emailable, Phoneable;
+    use Addressable, Emailable, Phoneable, Websiteable;
 }
